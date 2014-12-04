c1fc9714587883114e4027b264df82495f745a0e
deadkidsong <deadkidsong@hotmail.com>
2014-12-04 23:06:49 +0800

    init

---
+++ source/MenuState.hx
@@ -0,0 +1,47 @@
+package ;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.util.FlxMath;
+
+/**
+ * A FlxState which can be used for the game's menu.
+ */
+class MenuState extends FlxState
+{
+	/**
+	 * Function that is called up when to state is created to set it up.
+	 */
+	override public function create():Void
+	{
+		super.create();
+		
+		var buttomPlay:FlxButton = new FlxButton(100, 100, "Start", onBtnClk_play);
+		add(buttomPlay);
+	}
+	
+	private function onBtnClk_play() {
+		trace("play");
+		FlxG.switchState(new PlayState());
+	}
+
+	/**
+	 * Function that is called when this state is destroyed - you might want to
+	 * consider setting all objects this state uses to null to help garbage collection.
+	 */
+	override public function destroy():Void
+	{
+		super.destroy();
+	}
+
+	/**
+	 * Function that is called once every frame.
+	 */
+	override public function update():Void
+	{
+		super.update();
+	}
+}
---
+++ source/uni/UniParser.hx
@@ -0,0 +1,29 @@
+package uni;
+
+/**
+ * Override this to create your parser
+ * @author 
+ */
+class UniParser
+{
+
+	public function new() 
+	{
+		
+	}
+	
+	
+	//final
+	public function parseDataStr(String data):Void {
+			
+		var xml:Xml = Xml.parse(data);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
